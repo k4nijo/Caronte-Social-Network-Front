774d45df58454dc8152a548b9a666e4551f93797
Jonathan Pulido <jonathanpmelian@gmail.com>
2022-04-12 18:57:46 +0100

    PROFILE: My profile added

---
+++ pages/profile/bookmarks.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-container>
+    <PostResume :feed="bookmarks" />
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'mybookmarks',
+  data() {
+    return {
+      bookmarks: [],
+    }
+  },
+  async asyncData({ $axios }) {
+    let { data } = await $axios.get('/api/user/bookmarks')
+
+    return { bookmarks: data }
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
