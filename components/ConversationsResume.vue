d86fbd2d499ae26fa27c7c1e1271a71f8b69e9c8
Jonathan Pulido <89730979+jonathanpmelian@users.noreply.github.com>
2022-04-19 19:51:35 +0100

    Update ConversationsResume.vue

--- components/ConversationsResume.vue
+++ components/ConversationsResume.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-container class="pa-0 mx-auto size">
+  <v-container class="pa-0 mx-auto ">
     <v-card class="elevation-1">
       <v-row>
         <v-col class="titletext ml-2"> Recent Messages </v-col>
@@ -113,9 +113,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.size {
-  max-width: 800px;
-}
+
 .name {
   font-weight: bold;
 }
